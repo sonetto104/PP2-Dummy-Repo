4bca70c3a0380e7dfd1aa96ef9ccf8238bd727b7
sonetto104 <peter_regan88@yahoo.com>
2023-01-07 19:49:55 +0000

    correct code to be used in project.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,90 +1,226 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByClassName("submit-button");
-
-    
-    for (let button of buttons) {
-        button.addEventListener("click", function () {
-            if (this.getAttribute("id") === "quaver-button") {
-                let randomNote = notesArray[Math.floor(Math.random() * notesArray.length)];
-    let randomNoteTwo = notesArray[Math.floor(Math.random() * notesArray.length)];
-                randomNote.play();
-                setTimeout(function () {
-                    randomNoteTwo.play()
-                }, 1000);
-            } 
-            
-        })
-    }
-})
-    
-
-let randomNote = notesArray[Math.floor(Math.random() * notesArray.length)];
-let randomNoteTwo = notesArray[Math.floor(Math.random() * notesArray.length)];
-
-
 const intervalButton = document.getElementById("quaver-button");
-// const notes = document.getElementsByTagName("audio");
-// const notesArray = Array.from(notes);
-// let randomNote = notesArray[Math.floor(Math.random() * notesArray.length)];
-// let randomNoteTwo = notesArray[Math.floor(Math.random() * notesArray.length)];
-// let firstSemitoneValue = parseInt(randomNote.getAttribute("data-number"));
-// let secondSemitoneValue = parseInt(randomNoteTwo.getAttribute("data-number"));
-// let intervalNumber = firstSemitoneValue - secondSemitoneValue;
+const notes = document.getElementsByTagName("audio");
+const notesArray = Array.from(notes);
+let randomNotesArray = [];
 const submitButton = document.getElementById("submit");
+let intervalNumberContainer = [];
 
-
-
-// function getRandomNotes() {
-//     const notes = document.getElementsByTagName("audio");
-//     const notesArray = Array.from(notes);
-//     randomNote = notesArray[Math.floor(Math.random() * notesArray.length)];
-//     randomNoteTwo = notesArray[Math.floor(Math.random() * notesArray.length)];
-//     firstSemitoneValue = parseInt(randomNote.getAttribute("data-number"));
-//     secondSemitoneValue = parseInt(randomNoteTwo.getAttribute("data-number"));
-//     intervalNumber = firstSemitoneValue - secondSemitoneValue;
-//     randomNote.play();
-//     setTimeout(function () {
-//             randomNoteTwo.play()
-//         }, 1000);
-//     return intervalNumber;}
-
-    function getRandomNoteOne() {
-        const notes = document.getElementsByTagName("audio");
-        const notesArray = Array.from(notes);
-        let randomNoteOne = notesArray[Math.floor(Math.random() * notesArray.length)];
-        return randomNoteOne.play();
-    }
-
-    function getRandomNoteTwo() {
-        const notes = document.getElementsByTagName("audio");
-        const notesArray = Array.from(notes);
-        let randomNoteTwo = notesArray[Math.floor(Math.random() * notesArray.length)];
-        return randomNoteTwo.play();
-    }
-
-    function playTwoRandomNotes() {
-        getRandomNoteOne();
-        setTimeout(function () {
-            getRandomNoteTwo()
-        }, 1000);
-    }
+document.addEventListener("DOMContentLoaded", function() {
+        getTwoRandomNotes();
         
+})
+
+
+intervalButton.addEventListener("click", function() {
+    let randomNote = randomNotesArray[randomNotesArray.length - 2];
+    let randomNoteTwo = randomNotesArray[randomNotesArray.length - 1];
+    randomNote.play();
+    setTimeout(function () {
+        randomNoteTwo.play()
+    }, 1000);
+})
+
+
+const userIntervalQuality = document.getElementById('interval-quality');
+userIntervalQuality.addEventListener('change', function handleChange(event) {
+  (event.target.value);
+});
+
+const userIntervalNumber = document.getElementById("interval-number");
+  userIntervalNumber.addEventListener("change", function handleChange(event) {
+    (event.target.value);
+  })
 
 
 
 submitButton.addEventListener("click", function() {
-  switch (true) {
-    case intervalNumber === 0 && userIntervalQuality.value === "perfect" && userIntervalNumber.value === "unison":
-      alert("Perfect pitch, Puccini! Let's see how you handle the next interval.");
-      break;
-    case (intervalNumber === 1 || intervalNumber === -1) && userIntervalQuality.value === "minor" && userIntervalNumber.value === "2nd":
-      alert("Perfect pitch, Puccini! Let's see how you handle the next interval.");
-      break;
-    // Add cases for the remaining intervals
-    default:
-      alert("Sorry, that's not the correct interval. Try again!");
-  }
-});
+
+    if (intervalNumberContainer[intervalNumberContainer.length-1] === 0 && userIntervalQuality.options[userIntervalQuality.selectedIndex].value === "perfect"
+    && userIntervalNumber.options[userIntervalNumber.selectedIndex].value === "unison") {
+       alert("Perfect pitch, Puccini! Let's see how you handle the next interval.");
+       getTwoRandomNotes();
+       
+
+      } else if ((intervalNumberContainer[intervalNumberContainer.length-1] === 1 || intervalNumberContainer[intervalNumberContainer.length-1] === -1) && userIntervalQuality.options[userIntervalQuality.selectedIndex].value === "minor"
+   && userIntervalNumber.options[userIntervalNumber.selectedIndex].value === "2nd") {
+       alert("Perfect pitch, Puccini! Let's see how you handle the next interval.");
+       getTwoRandomNotes();
+       
+
+      } else if ((intervalNumberContainer[intervalNumberContainer.length-1] === 2 || intervalNumberContainer[intervalNumberContainer.length-1] === -2) && userIntervalQuality.options[userIntervalQuality.selectedIndex].value === "major"
+      && userIntervalNumber.options[userIntervalNumber.selectedIndex].value === "2nd") {
+          alert("Perfect pitch, Puccini! Let's see how you handle the next interval.");
+          getTwoRandomNotes();
+          
+
+      } else if ((intervalNumberContainer[intervalNumberContainer.length-1] === 3 || intervalNumberContainer[intervalNumberContainer.length-1] === -3) && userIntervalQuality.options[userIntervalQuality.selectedIndex].value === "minor"
+      && userIntervalNumber.options[userIntervalNumber.selectedIndex].value === "3rd") {
+          alert("Perfect pitch, Puccini! Let's see how you handle the next interval.");
+          getTwoRandomNotes();
+          
+         
+      } else if ((intervalNumberContainer[intervalNumberContainer.length-1] === 4 || intervalNumberContainer[intervalNumberContainer.length-1] === -4) && userIntervalQuality.options[userIntervalQuality.selectedIndex].value === "major"
+      && userIntervalNumber.options[userIntervalNumber.selectedIndex].value === "3rd") {
+          alert("Perfect pitch, Puccini! Let's see how you handle the next interval.");
+          getTwoRandomNotes();
+          
+          
+      } else if ((intervalNumberContainer[intervalNumberContainer.length-1] === 5 || intervalNumberContainer[intervalNumberContainer.length-1] === -5) && userIntervalQuality.options[userIntervalQuality.selectedIndex].value === "perfect"
+      && userIntervalNumber.options[userIntervalNumber.selectedIndex].value === "4th") {
+          alert("Perfect pitch, Puccini! Let's see how you handle the next interval.");
+          getTwoRandomNotes();
+          
+
+      } else if ((intervalNumberContainer[intervalNumberContainer.length-1] === 6 || intervalNumberContainer[intervalNumberContainer.length-1] === -6) && userIntervalQuality.options[userIntervalQuality.selectedIndex].value === "augmented"
+      && userIntervalNumber.options[userIntervalNumber.selectedIndex].value === "4th") {
+          alert("Perfect pitch, Puccini! Let's see how you handle the next interval.");
+          getTwoRandomNotes();
+          
+        
+      } else if ((intervalNumberContainer[intervalNumberContainer.length-1] === 6 || intervalNumberContainer[intervalNumberContainer.length-1] === -6) && userIntervalQuality.options[userIntervalQuality.selectedIndex].value === "diminished"
+      && userIntervalNumber.options[userIntervalNumber.selectedIndex].value === "5th") {
+          alert("Perfect pitch, Puccini! Let's see how you handle the next interval.");
+          getTwoRandomNotes();
+          
+          
+      } else if ((intervalNumberContainer[intervalNumberContainer.length-1] === 7 || intervalNumberContainer[intervalNumberContainer.length-1] === -7) && userIntervalQuality.options[userIntervalQuality.selectedIndex].value === "perfect"
+      && userIntervalNumber.options[userIntervalNumber.selectedIndex].value === "5th") {
+            alert("Perfect pitch, Puccini! Let's see how you handle the next interval.");
+            getTwoRandomNotes();
+            
+
+      } else if ((intervalNumberContainer[intervalNumberContainer.length-1] === 8 || intervalNumberContainer[intervalNumberContainer.length-1] === -8) && userIntervalQuality.options[userIntervalQuality.selectedIndex].value === "minor"
+      && userIntervalNumber.options[userIntervalNumber.selectedIndex].value === "6th") {
+          alert("Perfect pitch, Puccini! Let's see how you handle the next interval.");
+          getTwoRandomNotes();
+          
+
+      } else if ((intervalNumberContainer[intervalNumberContainer.length-1] === 9 || intervalNumberContainer[intervalNumberContainer.length-1] === -9) && userIntervalQuality.options[userIntervalQuality.selectedIndex].value === "major"
+      && userIntervalNumber.options[userIntervalNumber.selectedIndex].value === "6th") {
+          alert("Perfect pitch, Puccini! Let's see how you handle the next interval.");
+          getTwoRandomNotes();
+          
+
+      } else if ((intervalNumberContainer[intervalNumberContainer.length-1] === 10 || intervalNumberContainer[intervalNumberContainer.length-1] === -10) && userIntervalQuality.options[userIntervalQuality.selectedIndex].value === "minor"
+      && userIntervalNumber.options[userIntervalNumber.selectedIndex].value === "7th") {
+          alert("Perfect pitch, Puccini! Let's see how you handle the next interval.");
+          getTwoRandomNotes();
+          
+
+      } else if ((intervalNumberContainer[intervalNumberContainer.length-1] === 11 || intervalNumberContainer[intervalNumberContainer.length-1] === -11) && userIntervalQuality.options[userIntervalQuality.selectedIndex].value === "major"
+      && userIntervalNumber.options[userIntervalNumber.selectedIndex].value === "7th") {
+          alert("Perfect pitch, Puccini! Let's see how you handle the next interval.");
+          getTwoRandomNotes();
+          
+
+      } else if ((intervalNumberContainer[intervalNumberContainer.length-1] === 12 || intervalNumberContainer[intervalNumberContainer.length-1] === -12) && userIntervalQuality.options[userIntervalQuality.selectedIndex].value === "perfect"
+      && userIntervalNumber.options[userIntervalNumber.selectedIndex].value === "octave") {
+          alert("Perfect pitch, Puccini! Let's see how you handle the next interval.");
+          getTwoRandomNotes();
+          
+
+      } else {
+       alert("Not correct. Get busy, Bizet and try again!");
+      }
+})
+
+
+
+function getTwoRandomNotes() {
+    let randomNote = notesArray[Math.floor(Math.random() * notesArray.length)];
+    let randomNoteTwo = notesArray[Math.floor(Math.random() * notesArray.length)];
+    randomNotesArray.push(randomNote);
+    randomNotesArray.push(randomNoteTwo);
+
+    let firstSemitoneValue = parseInt(randomNote.getAttribute("data-number"));
+    let secondSemitoneValue = parseInt(randomNoteTwo.getAttribute("data-number"));
+    let intervalNumber = firstSemitoneValue - secondSemitoneValue;
+    intervalNumberContainer.push(intervalNumber);
+}
+
+// function calculateIntervalNumber() {
+//     let randomNote = randomNotesArray[randomNotesArray.length - 2];
+//     let randomNoteTwo = randomNotesArray[randomNotesArray.length - 1];
+//     let firstSemitoneValue = parseInt(randomNote.getAttribute("data-number"));
+//     let secondSemitoneValue = parseInt(randomNoteTwo.getAttribute("data-number"));
+//     let intervalNumber = firstSemitoneValue - secondSemitoneValue;
+//     intervalNumberContainer.push(intervalNumber);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+// submitButton.addEventListener("click", function() {
+//   switch (true) {
+//     case intervalNumber === 0 && userIntervalQuality.value === "perfect" && userIntervalNumber.value === "unison":
+//       alert("Perfect pitch, Puccini! Let's see how you handle the next interval.");
+//       break;
+//     case (intervalNumber === 1 || intervalNumber === -1) && userIntervalQuality.value === "minor" && userIntervalNumber.value === "2nd":
+//       alert("Perfect pitch, Puccini! Let's see how you handle the next interval.");
+//       break;
+//     // Add cases for the remaining intervals
+//     default:
+//       alert("Sorry, that's not the correct interval. Try again!");
+//   }
+// });
 
 
 
